6233e0618698b2d0fee58d81a092bcc8189fef57
tuetenhalter <tuetenhalter@gmail.com>
2025-10-06 03:49:20 +0200

    install.sh ask for every cmd

--- install.sh
+++ install.sh
@@ -1,52 +1,93 @@
+#!/bin/bash
 
+# Function to ask for confirmation
+ask_for_confirmation() {
+    local prompt_message="$1"
+    echo
+    read -r -p "$prompt_message (y/N): " response
+    case "$response" in
+        [yY][eE][sS]|[yY])
+            return 0 # Proceed
+            ;;
+        *)
+            echo "Skipping command."
+            return 1 # Skip
+            ;;
+    esac
+}
 
+# --- System Update ---
+echo "## Step 1: System Update"
+if ask_for_confirmation "Do you want to run 'sudo pacman -Syu' to update your system?"; then
+    echo "Running sudo pacman -Syu"
+    sudo pacman -Syu
+fi
 
-echo "run sudo pacman -Syu"
-sudo pacman -Syu
-
-
-
+# --- Package Installation ---
 packages=(
-	# hyprland 
-	hyprland  
-	qt5-wayland	
-	qt6-wayland
-	noto-fonts
-	hyprpolkitagent # Authentication Agent
-	dunst # notification daemon
+    # hyprland
+    hyprland
+    qt5-wayland
+    qt6-wayland
+    noto-fonts
+    hyprpolkitagent # Authentication Agent
+    dunst # notification daemon
 
-	kitty
-	dolphin
+    kitty
+    dolphin
+    zsh
 
-	# gui
-	rofi
-	waybar
+    # gui
+    rofi
+    waybar
 
-	# sound
-	pipewire
-	wireplumber
+    # sound
+    pipewire
+    wireplumber
 
-	#bluetooth
-	bluez
-	bluez-utils
-	bluetui
+    # bluetooth
+    bluez
+    bluez-utils
+    bluetui
 
-	#insterntet
-	nm-connection-editor
+    # internet
+    nm-connection-editor
 
-	# application
-	vim 
-	steam
-	firefox
-	discord
+    # application
+    vim
+    steam
+    firefox
+    discord
 )
 
 echo
-echo 
-echo "run sudo pacman -S ${packages[@]}"
-echo 
+echo "## Step 2: Package Installation"
+echo "The following packages will be installed: ${packages[@]}"
+if ask_for_confirmation "Do you want to run 'sudo pacman -S' to install these packages?"; then
+    echo "Running sudo pacman -S ${packages[@]}"
+    sudo pacman -S "${packages[@]}"
+fi
 
-sudo pacman -S "${packages[@]}"
+# --- Oh My Zsh Installation ---
+echo
+echo "## Step 3: Oh My Zsh Installation"
+if ask_for_confirmation "Do you want to install Oh My Zsh (runs a script from GitHub)?"; then
+    echo "Running sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\""
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-./copy.sh
+# --- Custom Copy Script ---
+echo
+echo "## Step 4: Run Custom Copy Script"
+if ask_for_confirmation "Do you want to copy the config files"; then
+    echo "Running ./copy.sh"
+    # Check if the script exists and is executable before running
+    if [ -x "./copy.sh" ]; then
+        ./copy.sh
+    else
+        echo "Error: './copy.sh' not found or not executable. Skipping."
+    fi
+fi
 
+echo
+echo "Script finished."

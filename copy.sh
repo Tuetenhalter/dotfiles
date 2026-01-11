da3be2ed7a10d3a2d64f29bbd0a5ab40280aab3b
Tuetenhalter <tuetenhalter@gmail.com>
2026-01-11 06:13:08 +0100

    cp script

--- copy.sh
+++ copy.sh
@@ -1,4 +1,13 @@
 
+echo "run copy"
 
-echo "run Copy"
-cp -r ./config/* ~/.config/
+# copy monitor if it dones exist
+if [ ! -f ~/.config/hypr/hyprland/monitors.conf ]; then
+    echo "copy monitor"
+    mkdir -p ~/.config/hypr/hyprland/
+    cp ./config/hypr/hyprland/monitors.conf ~/.config/hypr/hyprland/monitor
+fi
+
+# copys all files without monitor
+rsync -av --exclude 'hypr/hyprland/monitors.conf' ./config/ ~/.config/
+
